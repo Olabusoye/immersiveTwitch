452f096291b3f795b1e1a47c7a50142bfdccf4a3
Olabusoye <olabusoye.olaideabidoye@sky.uk>
2023-01-23 17:19:28 +0000

    website

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from 'react-bootstrap';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return<div className="App">
+    <header>
+      <h1>Sky Immersive Twitch Sports Stream</h1>
+      <p>Watch the sports you love with your favourite steamer!</p>
+    </header>
+    <Container className="streams">
+
+      <div className="twitchVid">
+        <iframe
+            src="https://player.twitch.tv/?channel=koyotitospa&parent=localhost" title="Twitch video"
+                allowFullScreen="true" height="500" width="800">
+        </iframe>
+      </div>
+      <div className="chat">
+        <iframe
+            id="chat_embed"
+            src="https://www.twitch.tv/embed/koyotitospa/chat?parent=localhost"
+            height="500"
+            width="350">
+        </iframe>
+      </div>
+      <div className="ytVid">
+        <iframe width="800" height="500" src="https://www.youtube.com/embed/o9bz3s5dG3U" title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                allowFullScreen></iframe>
+      </div>
+    </Container>
+
+
+
+  </div>
 }
 
 export default App;
